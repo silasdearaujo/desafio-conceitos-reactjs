d006b4fb93d0b9e80468bd80ee1e669216be0ec0
silas <silas.araujo@zema.com>
2020-12-30 12:54:21 -0300

    get/post

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+  
+  const [input, setInput] = useState('');
+
   async function handleAddRepository() {
-    // TODO
+    const response  = await api.post('/projects', {
+      title: `${input}`,
+      ownder: "Silas de Araujo"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
   }
-
+  
   return (
     <div>
+      <input value={input} onInput={e => setInput(e.target.value)}/>
+      <br/>
+      <button onClick={handleAddRepository}>Adicionar</button>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+        {projects.map(project => 
+        <li key={project.id}>{project.title}
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
